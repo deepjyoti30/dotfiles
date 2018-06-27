3aeb85ca0c2911a602e0ab01a87e43be083f989f
deepjyoti30 <deep.barman30@gmail.com>
2018-06-27 15:04:40 +0530

    added new bars

---
+++ .config/polybar/polybar/launch.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
+
+# Launch bars
+#polybar -r toolbar &
+polybar -r songbar &
+#polybar -r bottombar &
+polybar -r shortcuts &
+polybar -r util &
